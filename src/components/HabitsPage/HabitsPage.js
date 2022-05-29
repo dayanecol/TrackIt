dfdf1209b899018f70cb45f8bdb319985d6c9c7f
Dayane <dayanecarolina.oliveira@gmail.com>
2022-05-28 21:01:47 -0300

    History Page

--- src/components/HabitsPage/HabitsPage.js
+++ src/components/HabitsPage/HabitsPage.js
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+import Header from "../../shared/Header";
+import Footer from "../../shared/Footer";
+
 export default function HabitsPage (){
+
     return (
-        <h1>HabitsPage</h1>
+        <>
+            <Header />
+            <Habits>
+                <h2>Meus hábitos</h2>
+                <p>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</p>
+            </Habits>
+            <Footer />
+        </>
     );
 }
+
+const Habits = styled.div`
+    display:flex;
+    justify-content:flex-start;
+    align-items:center;
+    margin: 98px 18px 101px 18px;
+    flex-wrap:wrap;
+    h2{
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126BA5;
+    padding-bottom: 28px;
+    }
+    p{
+        font-size: 17.976px;
+        line-height: 22px;
+        color: var(--color-text);
+    }
+`;
--- src/components/HistoryPage/HistoryPage.js
+++ src/components/HistoryPage/HistoryPage.js
@@ -1,6 +1,38 @@
+import styled from "styled-components";
+import Header from "../../shared/Header";
+import Footer from "../../shared/Footer";
 
 export default function HistoryPage (){
     return (
-        <h1>HistoryPage</h1>
+        <>
+            <Header />
+            <History>
+                <h2>Histórico</h2>
+                <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+            </History>
+            <Footer />
+        </>
     );
 }
+
+
+const History = styled.div`
+    display:flex;
+    justify-content:flex-start;
+    align-items:center;
+    margin: 98px 18px 101px 18px;
+    flex-wrap:wrap;
+    h2{
+    font-weight: 400;
+    font-size: 22.976px;
+    line-height: 29px;
+    color: #126BA5;
+    padding-bottom: 28px;
+
+    }
+    p{
+        font-size: 17.976px;
+        line-height: 22px;
+        color: var(--color-text);
+    }
+`;
